64d86593657088e39dcb11ea94928b2928a08874
ShaheenHossain <rapidgrps@princegroup-bd.com>
2017-04-24 15:37:25 +0600

    Update __manifest__.py

--- medical/__manifest__.py
+++ medical/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Odoo Medical',
+    'name': 'Eagle Medical',
     'version': '10.0.1.0.0',
     'category': 'Medical',
     'depends': [
